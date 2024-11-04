1c4251252141b934d33ad3f024d69ed567ff3edc
André Coêlho <alcbcoelho@gmail.com>
2024-11-04 19:22:12 -0300

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Insira um número inteiro: ");
+        int n = sc.nextInt();
+        Integer n2 = n;
+        System.out.println("Número inserido: " + n2);
+    }
+}
